234fd3ff0c02ecd39435ee8d9ea48164e92b4f91
Anstice <s410378047@gm.ntpu.edu.tw>
2018-03-03 15:00:34 +0800

    從1-10 Okay

---
+++ app/src/main/java/com/example/student/a20180303_03asynctask/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.student.a20180303_03asynctask;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+
+        MyTask task = new MyTask();
+        task.execute(10);
+    }
+
+
+    class MyTask extends AsyncTask<Integer, Integer, String>{
+        @Override
+        protected String doInBackground(Integer... params){
+            int i;
+            for (i=1; i<=params[0]; i++)
+            {
+                try{
+                    Thread.sleep(500);
+                    //Log.d("TASK", "R:" + i );
+                    publishProgress(i);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            return "Okay";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.d("TASK", "progress:" + values[0]);
+            tv.setText(String.valueOf(values[0]));
+        }
+        @Override
+        protected void onPostExecute(String s) {                            //Execute 執行
+            super.onPostExecute(s);
+            Log.d("TASK", "post:" + s);
+            tv.setText(s);
+        }
+
+
+    }
+
+
+}
